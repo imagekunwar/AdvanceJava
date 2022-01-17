f30de25df02e125990b286e0fc4f839f8ea539fe
unknown <imagekunwar@gmail.com>
2022-01-17 09:00:23 +0545

    Coordinate System and 2D graphics in Java

--- GUIProgramming/src/edu/np/scst/first/DrawingShapes.java
+++ GUIProgramming/src/edu/np/scst/first/DrawingShapes.java
@@ -14,12 +14,22 @@ public class DrawingShapes extends JFrame {
           public void paint(Graphics g){
               Graphics2D g2=(Graphics2D)g;//typecast
               
+              Color c=new Color(0,255,255);
+              g2.setColor(c);
+              Color red=new Color(255,0,0);
+              g2.setPaint(red);
+              
               g2.drawLine(10, 10, 40, 40);
               g2.drawRect(50,10,30,50);
               g2.drawOval(70,70,55,55);
               g2.drawRect(180,180,180,180);
               g2.drawOval(10,90,50,30);
-              g2.drawArc(95,95,190,190,0,90);
+            //g2.drawArc(95,95,190,190,0,90);
+            g2.fillArc(95,95,190,190,0,90);
+              
+              int[]x={200,400,800};
+              int[]y={200,500,300};
+              g2.drawPolygon(x, y,3);
               
           }
       };
