31d3c194b7d148b6478730026b44ee470e6ead02
unknown <imagekunwar@gmail.com>
2022-01-23 08:16:51 +0545

    Displaying Image in Java Swing application

---
+++ GUIProgramming/src/edu/np/scst/first/DisplayingImage.java
@@ -0,0 +1,27 @@
+
+package edu.np.scst.first;
+import javax.swing.*;
+
+public class DisplayingImage extends JFrame{
+    DisplayingImage(){
+        ImageIcon image= new ImageIcon("img.png");
+        JPanel jPanel=new JPanel();
+        
+        JLabel jLabel=new JLabel(image);
+        add(jPanel);
+        add(jLabel);
+        
+        
+        
+        setVisible(true);
+        setSize(450,450);
+        setTitle("Displaying Image");
+        setDefaultCloseOperation(3);
+    }
+    
+    
+    public static void main(String[]args){
+        DisplayingImage d=new DisplayingImage();
+    }
+    
+}
