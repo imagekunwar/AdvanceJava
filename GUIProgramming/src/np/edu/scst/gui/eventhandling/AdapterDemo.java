1aa7aa94b50f29d29c5b48b38748cdb7ff194b4f
unknown <imagekunwar@gmail.com>
2022-01-28 09:01:25 +0545

    Adapter Class Concept

---
+++ GUIProgramming/src/np/edu/scst/gui/eventhandling/AdapterDemo.java
@@ -0,0 +1,32 @@
+
+package np.edu.scst.gui.eventhandling;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event;
+
+public class AdapterDemo extends JFrame {
+    AdapterDemo(){
+        JButton jButton=new JButton("Click me");
+        jButton.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseclicked(MouseEvent e){
+                System.out.println("The mouse was clicked");
+                        
+            }
+       
+          });
+        add(jButton);
+        setLayout(new FlowLayout());
+        setVisible(true);
+        setSize(400,400);
+        setDefaultCloseOperation(3);
+                
+    
+    
+    
+    
+    public static void main(String[]args){
+        new AdapterDemo();
+    }
+    
+}
