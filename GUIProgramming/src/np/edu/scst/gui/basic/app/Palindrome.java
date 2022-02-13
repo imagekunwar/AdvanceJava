87f0faef032f16b1f6c865d6e355ac123277586c
unknown <imagekunwar@gmail.com>
2022-02-13 23:03:27 +0545

    Palindrom error 80% solved

--- GUIProgramming/src/np/edu/scst/gui/basic/app/Palindrome.java
+++ GUIProgramming/src/np/edu/scst/gui/basic/app/Palindrome.java
@@ -2,19 +2,34 @@
 package np.edu.scst.gui.basic.app;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.MouseAdapter;
 
 public class Palindrome extends JFrame{
     
     Palindrome()
     {
         JLabel jLabel=new JLabel("Enter the number");
-        JTextField jTextField=new JTextField("*");
-        JButton jButton=new JButton("Result");
+        JTextField jTextField=new JTextField(20);
+        JButton jButton=new JButton("Check for palindrome");
+        JLabel jLabel1=new JLabel("*");
         
-        new
+        jButton.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e){
+                String input=jTextField.getText();
+                if(isPalindrome(input)){
+                    jLabel.setText("It is palindrome");
+                }else{
+                    jLabel.setText("It is not palindrome");
+                }
+            }
+        });
+        
         add(jLabel);
         add(jTextField);
         add(jButton);
+        add(jLabel1);
         
         
         
@@ -26,12 +41,11 @@ public class Palindrome extends JFrame{
         setTitle("Palindrome Check");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
-        
-        
-        
-        
-        
-    }
+        public boolean isPalindrome(String str){
+            StringBuilder temp=new StringBuilder(str);
+            temp=temp.reverse();
+            return str.equals(temp.toString());
+        }
     
     public static void main(String[]args){
         Palindrome p=new Palindrome();
