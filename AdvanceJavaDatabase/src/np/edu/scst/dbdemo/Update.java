cf4ffded304b83456ed1df7190cef18806909930
unknown <imagekunwar@gmail.com>
2022-02-14 09:08:34 +0545

    Update query successful

--- AdvanceJavaDatabase/src/np/edu/scst/dbdemo/Update.java
+++ AdvanceJavaDatabase/src/np/edu/scst/dbdemo/Update.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package np.edu.scst.dbdemo;
 
-/**
- *
- * @author Dell
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Update {
-    
+    public static void main(String[]args) throws Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con= 
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/ajpdb","root","");
+        
+        Statement stmnt=con.createStatement();
+        
+        String sql="UPDATE students SET program= 'BE' WHERE id= 1";
+        System.out.println(sql);
+        stmnt.execute(sql);
+        stmnt.close();
+        con.close();
+        
+        
+        
+    }
+
 }
