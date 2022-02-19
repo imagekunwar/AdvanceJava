2f75475b3f075383c59466652e6823dc07619834
unknown <imagekunwar@gmail.com>
2022-02-19 22:38:04 +0545

    Advance Java Lab File

---
+++ AdvanceJavaLab/src/advancejavalab/Lab1/Shapes.java
@@ -0,0 +1,21 @@
+
+package advancejavalab.Lab1;
+import javax.swing.*;
+
+public class Shapes extends JFrame {
+    Shapes(){
+        
+        
+        
+        
+        
+        setVisible(true);
+        setSize(400,400);
+        setTitle("Java Swing Shapes");
+        setDefaultCloseOperation(3);
+    }
+    public static void main(String[]args){
+        Shapes s=new Shapes();
+    }
+    
+}
