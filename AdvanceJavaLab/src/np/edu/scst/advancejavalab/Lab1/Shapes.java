96817c1f9d92a17f40ad1d086329e7245995b709
unknown <imagekunwar@gmail.com>
2022-03-06 08:09:53 +0545

    Servlet in Tomcat server

--- AdvanceJavaLab/src/np/edu/scst/advancejavalab/Lab1/Shapes.java
+++ AdvanceJavaLab/src/np/edu/scst/advancejavalab/Lab1/Shapes.java
@@ -14,6 +14,28 @@ public class Shapes extends JFrame {
             int x[]={0,90,180,90,180};
             int y[]={90,180,90,0,180};
             g2.drawPolygon(x,y,3);
+            
+            
+//           /? public void paint(Graphics g){
+              Graphics2D g2=(Graphics2D)g;//typecast
+              
+              Color c=new Color(0,255,255);
+              g2.setColor(c);
+              Color red=new Color(255,0,0);
+              g2.setPaint(red);
+              
+              g2.drawLine(10, 10, 40, 40);
+              g2.drawRect(50,10,30,50);
+              g2.drawOval(70,70,55,55);
+              g2.drawRect(180,180,180,180);
+              g2.drawOval(10,90,50,30);
+            //g2.drawArc(95,95,190,190,0,90);
+            g2.fillArc(95,95,190,190,0,90);
+              
+              int[]x={200,400,800};
+              int[]y={200,500,300};
+              g2.drawPolygon(x, y,3);
+              
                     
              
             
